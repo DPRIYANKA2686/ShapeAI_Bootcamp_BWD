8813a7bab7edfc8cbc3ba5ca07b5231f3bf56337
DPRIYANKA2686 <86555200+DPRIYANKA2686@users.noreply.github.com>
2021-06-27 16:54:40 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function info(){
+return (
+<div className="note">
+<h1>JAVASCRIPT AND REACT.JS</h1>
+<p>A basic web dev React Js Bootcamp</p>
+</div>
+);
+}
+
+export default info;
